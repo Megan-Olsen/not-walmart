01493926d344060e9a71f9386c4f57e35c2966b2
Megan Olsen <megan.a.olsen63@gmail.com>
2020-08-27 10:37:55 -0600

    stuff

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
+const express = require('express')
+const app = express ()
+const productCtrl = require('./controllers/productsCondroller')
 
+
+const SERVER_PORT = 5000
+
+
+
+app.use(express.json)
+
+
+
+app.listen(SERVER_PORT, () => 'Mindless consumerism on port ${SERVER_PORT}')
